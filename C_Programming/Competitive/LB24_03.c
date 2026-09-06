782e70042a49007c568be480eed5bfc699f974df
Mayur Shinde <mayurshinde0315@gmail.com>
2026-09-06 19:54:50 +0530

    Logic Building Using C Programming

---
+++ C_Programming/Competitive/LB24_03.c
@@ -0,0 +1,46 @@
+/*
+Accept number of rows and number of columns from user and display below
+pattern.
+Input : iRow = 3 iCol = 5
+Output : 
+A A A A A
+B B B B B
+C C C C C
+*/
+#include<stdio.h>
+void Pattern(int iRow , int iCol)
+{
+    int i = 0;
+    int j = 0 ;
+    char ch;
+
+    for(i=1, ch='A'; i<=iRow; i++)
+    {
+        for(j=1; j<=iCol; j++)
+        {
+            printf("%c\t", ch);
+    
+        }
+        ch++;
+     
+        printf("\n");
+    }
+}
+
+int main()
+
+{
+    int iValue1 = 0;
+    int iValue2 = 0;
+
+    printf("ENter the number of Row:");
+    scanf("%d", &iValue1);
+
+    printf("Enter the number of Coloums :");
+    scanf("%d",&iValue2);
+
+    Pattern(iValue1,iValue2);
+
+    return 0;
+
+}
